2b636dbe10bc7dd1b4ff3d53bc57115f8a6c31d7
Dominat0rr <claermandominik@gmail.com>
2021-08-27 01:08:20 +0200

    setup

---
+++ src/main/java/be/domain/Spring/Boot/and/Spring/Security/with/JWT/SpringBootAndSpringSecurityWithJwtApplication.java
@@ -0,0 +1,13 @@
+package be.domain.Spring.Boot.and.Spring.Security.with.JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndSpringSecurityWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndSpringSecurityWithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/domain/Spring/Boot/and/Spring/Security/with/JWT/SpringBootAndSpringSecurityWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package be.domain.Spring.Boot.and.Spring.Security.with.JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndSpringSecurityWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
